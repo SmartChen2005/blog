f71c9f290bb544bc7ac40bb5eb43a305f99410c8
Smart Chen <30527189+SmartChen2005@users.noreply.github.com>
2017-10-02 23:37:35 -0500

    Add files via upload

---
+++ works/扫雷小游戏/扫雷小游戏_files/demo.css
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="陈弘毅的网站">
+    
+    <script>
+        (function(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+        }
+        else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
+    </script>
+
+    
+    <title>Smart Chen</title>
+
+    <link rel="canonical" href="http://chenhongyi.cc/404.html">
+
+    <!-- Icons -->
+  <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/css/clean-blog.css">
+
+    <!-- Pygments Github CSS -->
+    <link rel="stylesheet" href="/css/syntax.css">
+
+    <!-- Custom Fonts -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<head>
+  <script>
+    console.log("Hello")
+  </script>  
+</head>  
+
+<body ontouchstart="">
+
+    <!-- Navigation -->
+<nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Smart Chen</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                
+                <li>
+                    <a href="/about/">About</a>
+                </li>
+                
+                <li>
+                    <a href="/archive/">Archive</a>
+                </li>
+                
+                <li>
+                    <a href="/lab/">Lab</a>
+                </li>
+                
+                <li>
+                    <a href="/milestone/">Milestone</a>
+                </li>
+                
+                <li>
+                    <a href="/search/">Search</a>
+                </li>
+                
+                <li>
+                    <a href="/tags/">Tags</a>
+                </li>
+                
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+
+    <!-- Page Header -->
+<head>
+    <script>
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?beb4251844e031fb200432d25f67c70c";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+    </script>
+    <script>(function(i,s,o,g,r,a,m){i["DaoVoiceObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;a.charset="utf-8";m.parentNode.insertBefore(a,m)})(window,document,"script",('https:' == document.location.protocol ? 'https:' : 'http:') + "//widget.daovoice.io/widget/f0bb3092.js","daovoice")
+    daovoice('init', {
+      app_id: "f0bb3092"
+    });
+    daovoice('update');
+    </script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-2665150688344645",
+        enable_page_level_ads: true
+      });
+    </script>
+</head>
+<header class="intro-header" style="background-image: url('/img/red.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="site-heading">
+                    <h1>Smart Chen</h1>
+                    <hr class="small">
+                    <span class="subheading">你来到了一片荒芜之地</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<link rel="stylesheet" href="/css/backtop.css">  <!-- Back Top -->
+<script type="text/javascript" src="/js/backtop.js"></script>  <!-- Back Top -->
+
+<div id="back-top">
+  <a href="#top" title="回到顶部"></a>
+</div>
+
+<!-- Main Content -->
+<div class="container">
+	<div class="row">
+		<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+			<pre>
+                       444444444       000000000            444444444   
+                      4::::::::4     00:::::::::00         4::::::::4  
+                     4:::::::::4   00:::::::::::::00      4:::::::::4  
+                    4::::44::::4  0:::::::000:::::::0    4::::44::::4  
+                   4::::4 4::::4  0::::::0   0::::::0   4::::4 4::::4  
+                  4::::4  4::::4  0:::::0     0:::::0  4::::4  4::::4  
+                 4::::4   4::::4  0:::::0     0:::::0 4::::4   4::::4  
+                4::::444444::::4440:::::0 000 0:::::04::::444444::::444
+                4::::::::::::::::40:::::0 000 0:::::04::::::::::::::::4
+                4444444444:::::4440:::::0     0:::::04444444444:::::444
+                          4::::4  0:::::0     0:::::0          4::::4  
+                          4::::4  0::::::0   0::::::0          4::::4  
+                          4::::4  0:::::::000:::::::0          4::::4  
+                        44::::::44 00:::::::::::::00         44::::::44
+                        4::::::::4   00:::::::::00           4::::::::4
+                        4444444444     000000000             4444444444
+</pre>
+
+<p>你来到这个页面，通常有两个原因</p>
+
+<h2 id="toc_0">一、链接错误</h2>
+
+<p>原因：博客搬迁造成的旧链接失效</p>
+
+<h2 id="toc_3">二、维度攻击</h2>
+
+<p>不明力量入侵，你想要访问的页面被摧毁了</p>
+
+</body>
+
+  <style>
+    pre {
+          background: none;
+          border: none;
+    }
+  </style>
+
+  <pre>         
+---x-----mm--
+      /^(  )^\
+      \,(..),/        Oops...
+        V~~V                     
+一定是发生了什么可怕的事情....
+    </pre>
+
+
+		</div>
+	</div>
+</div>
+
+<hr>
+
+
+    <!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <ul class="list-inline text-center">
+                    <!-- kill the Facebook and Weibo -->
+                    <li>
+                        <a href="/feed.xml">
+                            <span class="fa-stack fa-lg">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-rss fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    
+
+                    
+                    <li>
+                        <a href="https://github.com/SmartChen2005">
+                            <span class="fa-stack fa-lg">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    
+                    
+                    <!--
+                    
+                    -->
+
+                    <!--
+                    
+                    -->
+
+                    
+                    <li>
+                        <a target="_blank" href="https://www.zhihu.com/people/smart-90-99">
+                            <span class="fa-stack fa-lg">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa  fa-stack-1x fa-inverse">知</i>
+                            </span>
+                        </a>
+                    </li>
+                    
+                    <!--
+                    
+                    -->
+
+                </ul>
+                <p class="copyright text-muted">
+                <a href="/chenhongyi.cc.url">
+                    将本站添加到桌面
+                </a>
+                <a href="/email-login">
+                E-mail login
+                </a>
+                    <br>
+                    友情链接
+                    <br>
+                    <a href="http://www.rdfzcygj.cn">人大附中朝阳分校<a>
+                    <br>
+                    <a href="http://www.wulehao.com">邬乐昊的主页</a>
+                </p>     
+                <p class="copyright text-muted">
+                &copy; 2017  Smart Chen
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="/js/jquery.min.js "></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="/js/bootstrap.min.js "></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="/js/clean-blog.min.js "></script>
+
+
+
+<!-- Highlight.js -->
+<script>
+    async("http://cdn.bootcss.com/highlight.js/8.6/highlight.min.js",function(){
+        hljs.initHighlightingOnLoad();
+    })
+</script>
+<link href="http://cdn.bootcss.com/highlight.js/8.6/styles/github.min.css" rel="stylesheet">
+
+
+</body>
+
+</html>
