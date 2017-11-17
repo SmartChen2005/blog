77d2da7713227aea836c6ff22ad1b9db7fd5fbbd
Smart Chen <15811561198@163.com>
2017-11-17 19:41:34 +0800

    none

--- lab/Smart的机器助理.py
+++ lab/Smart的机器助理.py
@@ -1,18 +1,12 @@
-"""
-用Python3运行此程序，即可与Smart的机器助理聊天！
-
-more on www.chenhongyi.cc/lab/
-
-"""
 import requests
 user = input("Enter user name:")
 print("Smart的机器助理:你好,"+user+"!我是Smart的机器助理")
-while 1:
+while True:
     s = input(user+":")
     resp = requests.post("http://www.tuling123.com/openapi/api", data={
         "key": "dfa21a126ed2468ca2b9f49d27d7356e",
         "info": s,
-        "userid": "126531"
+        "userid": user
     })
     resp = resp.json()
     print("Smart的机器助理:"+resp['text'])
